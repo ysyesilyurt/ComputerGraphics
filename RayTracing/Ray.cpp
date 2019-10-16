f19612f9a5f0640c078bbf0d13fcb9d037e9c056
gokhansan <san.gokhan97@gmail.com>
2019-10-16 20:09:27 +0300

    vector arithmetic moved to helpers.h & dot and cross products implemented

--- RayTracing/Ray.cpp
+++ RayTracing/Ray.cpp
@@ -1,4 +1,5 @@
 #include "Ray.h"
+#include "helpers.h"
 
 Ray::Ray()
 {
@@ -9,92 +10,17 @@ Ray::Ray(const Vector3f& origin, const Vector3f& direction)
 {
 }
 
-
-// Helper function for addition
-Vector3f Ray::vectorSum(Vector3f first, Vector3f second, int type) const
-{
-	Vector3f res;
-
-	if(type == 1)
-	{
-		res.r = first.r + second.r;
-		res.g = first.g + second.g;
-		res.b = first.b + second.b;
-	}
-	else 
-	{
-		res.x = first.x + second.x;
-		res.y = first.y + second.y;
-		res.z = first.z + second.z;
-	}
-
-	return res;
-}
-
-Vector3f Ray::vectorSubtract(Vector3f first, Vector3f second, int type) const
-{
-	Vector3f res;
-
-	if(type == 1)
-	{
-		res.r = first.r - second.r;
-		res.g = first.g - second.g;
-		res.b = first.b - second.b;
-	}
-	else 
-	{
-		res.x = first.x - second.x;
-		res.y = first.y - second.y;
-		res.z = first.z - second.z;
-	}
-
-	return res;
-}
-
-Vector3f Ray::scalarMultipleVector(float scale, Vector3f vector, int type) const
-{
-	Vector3f res;
-
-	if(type == 1)
-	{
-		res.r = scale * vector.r;
-		res.g = scale * vector.g;
-		res.b = scale * vector.b;
-	}
-	else 
-	{
-		res.x = scale * vector.x;
-		res.y = scale * vector.y;
-		res.z = scale * vector.z;
-	}
-
-	return res;
-}
-
-
-
 /* Takes a parameter t and returns the point accoring to t. t is the parametric variable in the ray equation o+t*d.*/
 Vector3f Ray::getPoint(float t) const 
 {
-	/***********************************************
-     *                                             *
-	 * TODO: Implement this function               *
-     *                                             *
-     ***********************************************
-	 */
-    return this->vectorSum( this->origin, this->scalarMultipleVector(t, this->direction));
+    return vectorSum( this->origin, scalarMultipleVector(t, this->direction));
 }
 
 /* Takes a point p and returns the parameter t according to p such that p = o+t*d. */
 float Ray::gett(const Vector3f & p) const
 {
-	/***********************************************
-     *                                             *
-	 * TODO: Implement this function               *
-     *                                             *
-     ***********************************************
-	 */
-    Vector3f diff = this->vectorSubtract(p,this->origin);
+    Vector3f diff = vectorSubtract(p,this->origin);
+    
     return (diff.x + diff.y + diff.z)  / (this->direction.x + this->direction.y + this->direction.z);
 }
 
--- RayTracing/Ray.h
+++ RayTracing/Ray.h
@@ -18,9 +18,6 @@ public:
 
 private:
 	// Write any other stuff here. 
-	Vector3f scalarMultipleVector(float scale, const Vector3f vector, int type = 0) const;
-	Vector3f vectorSubtract(Vector3f first, const Vector3f second, int type = 0) const;
-	Vector3f vectorSum(Vector3f first, const Vector3f second, int type = 0) const;
 };
 
 #endif
