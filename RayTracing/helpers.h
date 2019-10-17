f962d39cd3e9738b2e9701798595e312d1d0e75d
ysyesilyurt <yesilyurt.selim@metu.edu.tr>
2019-10-17 23:59:37 +0300

    updated existing functionality accordingly with operator impls

--- RayTracing/helpers.h
+++ RayTracing/helpers.h
@@ -5,87 +5,87 @@
 #include "defs.h"
 
 
-inline Vector3f vectorSum(const Vector3f & first, const Vector3f & second, int type = 0)
-{
-	Vector3f res;
-
-	if(type == 1)
-	{
-		res.r = first.r + second.r;
-		res.g = first.g + second.g;
-		res.b = first.b + second.b;
-	}
-	else 
-	{
-		res.x = first.x + second.x;
-		res.y = first.y + second.y;
-		res.z = first.z + second.z;
-	}
-
-	return res;
-}
-
-
-inline Vector3f vectorSubtract(const Vector3f & first, const Vector3f & second, int type = 0)
-{
-	Vector3f res;
-
-	if(type == 1)
-	{
-		res.r = first.r - second.r;
-		res.g = first.g - second.g;
-		res.b = first.b - second.b;
-	}
-	else 
-	{
-		res.x = first.x - second.x;
-		res.y = first.y - second.y;
-		res.z = first.z - second.z;
-	}
-
-	return res;
-}
-
-
-inline Vector3f scalarMultipleVector(float scale, const Vector3f & vector, int type = 0)
-{
-	Vector3f res;
-
-	if(type == 1)
-	{
-		res.r = scale * vector.r;
-		res.g = scale * vector.g;
-		res.b = scale * vector.b;
-	}
-	else 
-	{
-		res.x = scale * vector.x;
-		res.y = scale * vector.y;
-		res.z = scale * vector.z;
-	}
-
-	return res;
-}
-
-inline Vector3f scalarDivisionVector(float scale, const Vector3f & vector, int type = 0)
-{
-    Vector3f res;
-
-    if(type == 1)
-    {
-        res.r = vector.r / scale;
-        res.g = vector.g / scale;
-        res.b = vector.b / scale;
-    }
-    else
-    {
-        res.x = vector.x / scale;
-        res.y = vector.y / scale;
-        res.z = vector.z / scale;
-    }
-
-    return res;
-}
+//inline Vector3f vectorSum(const Vector3f & first, const Vector3f & second, int type = 0)
+//{
+//	Vector3f res;
+//
+//	if(type == 1)
+//	{
+//		res.r = first.r + second.r;
+//		res.g = first.g + second.g;
+//		res.b = first.b + second.b;
+//	}
+//	else
+//	{
+//		res.x = first.x + second.x;
+//		res.y = first.y + second.y;
+//		res.z = first.z + second.z;
+//	}
+//
+//	return res;
+//}
+//
+//
+//inline Vector3f vectorSubtract(const Vector3f & first, const Vector3f & second, int type = 0)
+//{
+//	Vector3f res;
+//
+//	if(type == 1)
+//	{
+//		res.r = first.r - second.r;
+//		res.g = first.g - second.g;
+//		res.b = first.b - second.b;
+//	}
+//	else
+//	{
+//		res.x = first.x - second.x;
+//		res.y = first.y - second.y;
+//		res.z = first.z - second.z;
+//	}
+//
+//	return res;
+//}
+//
+//
+//inline Vector3f scalarMultipleVector(float scale, const Vector3f & vector, int type = 0)
+//{
+//	Vector3f res;
+//
+//	if(type == 1)
+//	{
+//		res.r = scale * vector.r;
+//		res.g = scale * vector.g;
+//		res.b = scale * vector.b;
+//	}
+//	else
+//	{
+//		res.x = scale * vector.x;
+//		res.y = scale * vector.y;
+//		res.z = scale * vector.z;
+//	}
+//
+//	return res;
+//}
+//
+//inline Vector3f scalarDivisionVector(float scale, const Vector3f & vector, int type = 0)
+//{
+//    Vector3f res;
+//
+//    if(type == 1)
+//    {
+//        res.r = vector.r / scale;
+//        res.g = vector.g / scale;
+//        res.b = vector.b / scale;
+//    }
+//    else
+//    {
+//        res.x = vector.x / scale;
+//        res.y = vector.y / scale;
+//        res.z = vector.z / scale;
+//    }
+//
+//    return res;
+//}
 
 inline float dotProduct(const Vector3f & first, const Vector3f & second)
 {
@@ -111,7 +111,8 @@ inline void normalize(Vector3f & vector) { // TODO & ?
     float len = length(vector);
     if (len == 0.0f)
         vector = {0,0,0}; // len is zero then return zero vector
-    vector = scalarDivisionVector(len, vector);
+//    vector = scalarDivisionVector(len, vector);
+    vector = vector / len;
 }
 
 
