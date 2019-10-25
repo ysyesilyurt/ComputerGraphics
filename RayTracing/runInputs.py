db5c2bd859efa6419fa5208dd1bc8fff0beb56f3
ysyesilyurt <yesilyurt.selim@metu.edu.tr>
2019-10-25 18:39:24 +0300

    .gitignore etc

--- RayTracing/runInputs.py
+++ RayTracing/runInputs.py
@@ -27,7 +27,7 @@ def runTests():
         if retCode:
             print("Oops, couldn't render {}, sth went wrong...".format(i))
             exit(0)
-        print('{0} took {1:.3f} ms = {2:.3f} s.'.format(i, elapsed * 1000.0, elapsed))
+        print('{0} took {1:.3f} ms = {2:.3f} s = {3:.3f} mins.'.format(i, elapsed * 1000.0, elapsed, elapsed/60))
 
 
 if __name__ == "__main__":
